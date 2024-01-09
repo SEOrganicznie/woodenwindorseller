ec6dc0566fbcf068fbd105dee8d3cabe0249c25f
Karinoko <xblexys@gmail.com>
2024-01-09 11:14:51 +0100

    header-footer update
    header i footer są od tej pory ładowany z osobnego pliku

--- js/script.js
+++ js/script.js
@@ -1,10 +1,47 @@
-window.onload = function() {
-    if (!localStorage.getItem('cookies-read')) {
-      document.getElementById('cookies-container').style.display = 'flex';
+window.onload = function()
+{
+  includeHTML();
+  console.log("run ciasteczka");
+  if (!localStorage.getItem('cookies-read'))
+  {
+    document.getElementById('cookies-container').style.display = 'flex';
+  }
+}
+
+function closeCookieInfo()
+{
+  console.log("run close ciasteczka");
+  localStorage.setItem('cookies-read', 'true');
+  document.getElementById('cookies-container').style.display = 'none';
+}
+
+function includeHTML()
+{
+  console.log("run code");
+  var code = "get-html";
+  var error404 = "Nie znaleziono takiej strony";
+  var elements, i, element, file, xhttp;
+  elements = document.getElementsByTagName("*");
+  for (i = 0; i < elements.length; i++)
+  {
+    element = elements[i];
+    file = element.getAttribute(code);
+    if (file)
+    {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function()
+      {
+        if (this.readyState == 4)
+        {
+          if (this.status == 200) {element.innerHTML = this.responseText;}
+          if (this.status == 404) {element.innerHTML = error404;}
+          element.removeAttribute(code);
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
     }
   }
-
-  function closeCookieInfo() {
-    localStorage.setItem('cookies-read', 'true');
-    document.getElementById('cookies-container').style.display = 'none';
-  }
+};
